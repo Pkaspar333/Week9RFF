82a8fe469d9a2b9c7510839441a6b968e4066833
Kaspar P <kaspar.parve@tktk.ee>
2024-12-18 18:26:11 +0200

    week 9

---
+++ Week9RFF/Program.cs
@@ -0,0 +1,51 @@
+﻿
+string fileDirectoryPath = @"C:\Users\Kaspar P\Documents\Programming\Week 9";
+string fileName = "chuck.txt";
+
+string fullFilePath = Path.Combine(fileDirectoryPath, fileName);
+
+ReadFromFile(fullFilePath);
+
+static void DisplayDataFromArry(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+static void ReadFromFile(string filePath)
+{
+    if (File.Exists(filePath))
+    {
+        string[] tempArray = File.ReadAllLines(filePath);
+        Console.WriteLine($"There are {tempArray.Length} lines in the file.");
+        Console.WriteLine("Do you want to see all lines or pick a random one? all/random");
+        string userChoise = Console.ReadLine();
+
+        if (userChoise == "all")
+        {
+            DisplayDataFromArry(tempArray);
+        }
+        else if (userChoise == "random")
+        {
+            DisplayRandomElement(tempArray);
+        }
+        else
+        {
+            Console.WriteLine("Try again.");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"File was not found.");
+    }
+}
+
+static void DisplayRandomElement(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    Console.WriteLine(someArray[randomIndex]);
+}
